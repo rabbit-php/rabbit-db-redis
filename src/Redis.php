6515efe00273ac38fdd1a7c7f1fd678a4c993536
albert <amuluowin@163.com>
2020-06-09 21:46:31 +0800

    remove interface

--- src/Redis.php
+++ src/Redis.php
@@ -3,12 +3,10 @@
 
 namespace rabbit\db\redis;
 
-use rabbit\db\ConnectionInterface;
-
 /**
  * Class Redis
  * @package rabbit\db\redis
  */
-class Redis extends SwooleRedis implements ConnectionInterface
+class Redis extends SwooleRedis
 {
 }
