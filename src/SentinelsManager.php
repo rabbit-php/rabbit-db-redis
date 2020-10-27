791261a7e9e20c7d07e140e175a3c4257d6e0f60
Albert <amuluowin@163.com>
2020-10-27 17:54:01 +0800

    opt code

--- src/SentinelsManager.php
+++ src/SentinelsManager.php
@@ -47,7 +47,6 @@ class SentinelsManager
             $key = $sentinel['hostname'] . (isset($sentinel['port']) ? ':' . $sentinel['port'] : '');
 
             if ($this->current >= $size) {
-                waitChannel($this->channel);
                 $connection = $this->channel->pop();
             } else {
                 try {
