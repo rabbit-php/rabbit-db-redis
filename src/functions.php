94e42ee9a989d9360de16fde1f6c8c01420ca384
Albert <amuluowin@163.com>
2021-04-20 00:20:16 +0800

    opt name

--- src/functions.php
+++ src/functions.php
@@ -8,9 +8,9 @@ use Rabbit\DB\Redis\RedisLock;
 if (!function_exists('rlock')) {
     function rlock(callable $function, bool $next = true, string $key = '', float $timeout = 600, string $name = 'default')
     {
-        if (null === $lock = LockHelper::getLock('redis')) {
+        if (null === $lock = LockHelper::getLock("redis:$name")) {
             $lock = new RedisLock(getDI('redis')->get($name));
-            LockHelper::add('redis', $lock);
+            LockHelper::add("redis:$name", $lock);
         }
         return $lock($function, $next, $key, $timeout);
     }
