8f2ca9997a99443c79962d80413a695799abe22c
Albert <amuluowin@163.com>
2022-04-22 12:37:10 +0800

    opt lock

--- src/RedisLock.php
+++ src/RedisLock.php
@@ -7,7 +7,6 @@ namespace Rabbit\DB\Redis;
 use Closure;
 use Rabbit\Base\App;
 use Rabbit\Base\Contract\LockInterface;
-use Rabbit\Base\Core\Channel;
 use Rabbit\Base\Helper\ExceptionHelper;
 use Throwable;
 
@@ -21,38 +20,21 @@ final class RedisLock implements LockInterface
         $this->redis = $redis ?? service('redis')->get();
     }
 
-    public function __invoke(Closure $function, bool $next = true, string $name = '', float $timeout = 600)
+    public function __invoke(string $name, Closure $function, bool $next = true, float $timeout = 600): void
     {
-        $name = "lock:" . (empty($name) ? uniqid() : $name);
-        if (!($this->channel[$name] ?? false)) {
-            $channel = new Channel();
-            $this->channel[$name] = $channel;
-        } else {
-            $channel = $this->channel[$name];
-        }
-        try {
-            if ($channel->isFull() && !$next) {
-                return false;
-            }
-            if ($channel->push(1, $timeout)) {
+        $name = "lock:{$name}";
+        lock($name, function () use ($name, $timeout, $function): void {
+            try {
                 $nx = $timeout > 0 ? ['NX', 'EX' => (int)$timeout] : ['NX'];
-                if ($this->redis->set($name, true, $nx) === null) {
-                    if ($next) {
-                        $this->redis->brpop("{$name}_list", (int)$timeout);
-                    } else {
-                        return false;
-                    }
+                if ($this->redis->set($name, $name, $nx) === null) {
+                    $this->redis->brpop("{$name}_list", (int)$timeout); 
                 }
+                $function();
+            } catch (Throwable $throwable) {
+                App::error(ExceptionHelper::dumpExceptionToString($throwable));
+            } finally {
+                $this->redis->rpush("{$name}_list", $name);
             }
-            return $function();
-        } catch (Throwable $throwable) {
-            App::error(ExceptionHelper::dumpExceptionToString($throwable));
-        } finally {
-            $this->redis->del($name);
-            $this->redis->rpush("{$name}_list", $name);
-            if ($channel->isFull()) {
-                $channel->pop();
-            }
-        }
+        }, $next, (int)$timeout);
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -6,12 +6,12 @@ use Rabbit\Base\Helper\LockHelper;
 use Rabbit\DB\Redis\RedisLock;
 
 if (!function_exists('rlock')) {
-    function rlock(string $key, callable $function, bool $next = true, float $timeout = 600, string $name = 'default')
+    function rlock(string $key, callable $function, bool $next = true, float $timeout = 600, string $name = 'default'): void
     {
         if (null === $lock = LockHelper::getLock("redis:$name")) {
             $lock = new RedisLock(service('redis')->get($name));
             LockHelper::add("redis:$name", $lock);
         }
-        return $lock($function, $next, $key, $timeout);
+        $lock($key, $function, $next, $timeout);
     }
 }
